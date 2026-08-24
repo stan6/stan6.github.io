b120f92531731264387788a0f8bc1588be7d3879
Shin Hwei Tan <159520+stan6@users.noreply.github.com>
2026-08-24 17:01:21 -0400

    Update sw.js

--- french-hunt-quest/sw.js
+++ french-hunt-quest/sw.js
@@ -1,4 +1,4 @@
-const CACHE="fhq-v1";
+const CACHE = "fhq-v2";
 const CORE=["./","./index.html","./styles.css","./data.js","./app.js","./manifest.webmanifest",
 "./assets/app-icon.png","./assets/intro.png","./assets/find-treasure.png","./assets/learn-french.png","./assets/create-adventure.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
